05f51529ceb3d569c44f8874c5d6fbb8da301bcc
junho <junho4051282@gmail.com>
2022-07-28 16:08:47 +0900

    v16

--- pQ2.py
+++ pQ2.py
@@ -1,5 +1,4 @@
 N = int(input('사이클을 알고 싶은 정수 N을 입력하시오. : '))
-infi=0
 count=0
 
 if N<0 or 99<N:
@@ -9,7 +8,7 @@ else:
     N = ((N%10) * 10)+(((N//10)+(N%10))%10)
     count=1
 
-while infi==0:
+while True:
     if first_N == N :
         break
     else :
