d8b4ac1c58d49fb8342d7fe55a14e8995776e994
junho <junho4051282@gmail.com>
2022-07-28 17:10:12 +0900

    v18

--- pQ4.py
+++ pQ4.py
@@ -1,11 +1,15 @@
 data = input('숫자로 이루어진 문자열을 입력하세요. ')
 
-result = 1
+result = int(data[0])
 
-for i in range(0, len(data)):
-    if int(data[i])==0 or int(data[i])==1:
+print(result, end=' ')
+
+for i in range(1, len(data)):
+    if int(data[i])<=1 or result <=1:
         result += int(data[i])
+        print('+ '+ data[i], end=' ')
     else:
         result = result*int(data[i])
+        print('x '+ data[i], end=' ')
 
-print(result)
+print(' = ' + str(result))
