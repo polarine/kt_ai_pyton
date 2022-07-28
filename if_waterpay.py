0159a3e84d2936277bc37ef7698e4bd019e922db
junho <junho4051282@gmail.com>
2022-07-28 11:16:24 +0900

    v 10

--- if_waterpay.py
+++ if_waterpay.py
@@ -2,24 +2,20 @@ def waterPay(a, b):
     
     if a == 'A':
         pay = 100 * b
-        return pay
     elif a == 'B':
         if b < 50 :
             pay = b * 150
-            return pay
         else :
             pay = (150 * 50) + (75 * (b-50))
-            return pay
     elif a == 'a':
         pay = 100 * b
-        return pay
     elif a == 'b':
         if b < 50 :
             pay = b * 150
-            return pay
         else :
             pay = (150 * 50) + (75 * (b-50))
-            return pay
+            
+    return pay
         
 print ('수도 요금을 계산하겠습니다.')
 a = input('수도 회사를 입력해주세요. : ')
