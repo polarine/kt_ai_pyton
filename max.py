d38d67a7e34fe15ab63b7289ad447f8da4ff9239
junho <junho4051282@gmail.com>
2022-07-28 09:54:23 +0900

    v2

---
+++ max.py
@@ -0,0 +1,12 @@
+print('세 정수의 최댓값을 구합니다')
+a= int(input('정수a의 값을 입력하세요.:'))
+b= int(input('정수b의 값을 입력하세요.:'))
+c= int(input('정수c의 값을 입력하세요.:'))
+
+maximum = a
+if b > maximum:
+    maximum = b
+if c > maximum:
+    maximum = c
+    
+print(f'최대값은 {maximum}입니다.')
