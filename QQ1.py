014d30b03060c9bf6b6c532229a2af930a791c49
junho <junho4051282@gmail.com>
2022-07-29 15:06:41 +0900

    v 20

--- QQ1.py
+++ QQ1.py
@@ -11,8 +11,36 @@ def solution(lottos, win_nums):
                 if i == j:
                     hit += 1
     
-    answer.append(hit)
-    answer.append(hit+z_num)
+    max_hit = z_num+hit
+    
+    if hit <=1:
+        minimum = 6
+    elif hit == 2:
+        minimum = 5
+    elif hit == 3:
+        minimum = 4
+    elif hit == 4:
+        minimum = 3
+    elif hit == 5:
+        minimum = 2
+    elif hit == 6:
+        minimum = 1
+    
+    if max_hit <=1:
+        maximum = 6
+    elif max_hit == 2:
+        maximum = 5
+    elif max_hit == 3:
+        maximum = 4
+    elif max_hit == 4:
+        maximum = 3
+    elif max_hit == 5:
+        maximum = 2
+    elif max_hit == 6:
+        maximum = 1
+    
+    answer.append(maximum)
+    answer.append(minimum)
     return answer
                 
 lottos = [44, 1, 0, 0, 31, 25]
